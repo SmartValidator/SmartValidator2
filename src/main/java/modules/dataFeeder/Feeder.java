ab5e6b064256dc4406656e0dca54f82e51362b07
yafim kazak <yafim.kazak@gmail.com>
2017-12-29 14:29:14 +0200

    added feeder

---
+++ src/main/java/modules/dataFeeder/Feeder.java
@@ -0,0 +1,28 @@
+package modules.dataFeeder;
+
+import java.net.URL;
+
+public class Feeder {
+    private static Feeder instance = null;
+    private RpkiFeederControlThread rpkiFeederControlThread = null;
+
+    private Feeder() {
+    }
+
+    public void start() {
+        URL url = getClass().getResource("/rpki-validator-3/rpki-validator-3.0.0-SNAPSHOT.jar");
+        rpkiFeederControlThread = new RpkiFeederControlThread(url.getPath());
+        rpkiFeederControlThread.start();
+    }
+
+    public void close() {
+        rpkiFeederControlThread.stop();
+    }
+
+    public static Feeder getInstance() {
+        if (instance == null) {
+            instance = new Feeder();
+        }
+        return instance;
+    }
+}
---
+++ src/main/java/modules/dataFeeder/RpkiFeederControlThread.java
@@ -0,0 +1,42 @@
+package modules.dataFeeder;
+
+import java.io.IOException;
+
+    public class RpkiFeederControlThread implements Runnable {
+        public Thread thread_handle;
+        private String rpki_validator_jar_path;
+
+        RpkiFeederControlThread(String rpki_validator_jar_path){
+
+            this.rpki_validator_jar_path = rpki_validator_jar_path;
+        }
+
+        public void run(){
+            ProcessBuilder pb = new ProcessBuilder("java", "-jar",rpki_validator_jar_path);
+            try {
+                Process p = pb.start();
+                p.waitFor();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        public void start () {
+            System.out.println("Starting ");
+            if (thread_handle == null) {
+                thread_handle = new Thread (this);
+                thread_handle.start ();
+            }
+        }
+
+        public void stop (){
+            System.out.println("Stopping ");
+            if (thread_handle != null) {
+                thread_handle.interrupt();
+            }
+        }
+    }
+
