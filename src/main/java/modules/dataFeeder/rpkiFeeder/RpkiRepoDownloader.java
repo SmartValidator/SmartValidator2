aaee77f4a7b70aafdd0bcdf40e8af13b8db45850
yafim kazak <yafim.kazak@gmail.com>
2018-01-02 08:24:39 +0200

    ADDED HTTP CLIENT

--- src/main/java/modules/dataFeeder/rpkiFeeder/RpkiRepoDownloader.java
+++ src/main/java/modules/dataFeeder/rpkiFeeder/RpkiRepoDownloader.java
@@ -1,9 +1,34 @@
 package modules.dataFeeder.rpkiFeeder;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.client.HttpResponse;
+import io.vertx.ext.web.client.WebClient;
+import io.vertx.ext.web.codec.BodyCodec;
+import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
 
 public class RpkiRepoDownloader implements Runnable {
 
+    private Vertx vertx;
+
+    public RpkiRepoDownloader() {
+    }
+
     @Override
     public void run() {
+        WebClient client = WebClient.create(vertx);
 
+        client
+                .get(9176, "localhost", "/export.json")
+                .send(ar -> {
+                    if (ar.succeeded()) {
+                        // Obtain response
+                        HttpResponse<Buffer> response = ar.result();
+                        System.out.println("Got HTTP response body");
+                        System.out.println(response.body().encodePrettily());
+                    } else {
+                        System.out.println("Something went wrong " + ar.cause().getMessage());
+                    }
+                });
     }
 }
