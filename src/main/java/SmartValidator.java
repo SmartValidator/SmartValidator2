4879896f1a5a320ee7c9e19b89123c56b51536c6
Tomas Hlavacek <tmshlvck@gmail.com>
2018-01-04 15:29:42 +0100

    Experimental simulator main added

--- src/main/java/SmartValidator.java
+++ src/main/java/SmartValidator.java
@@ -1,10 +1,12 @@
 import modules.dataFeeder.Feeder;
 import modules.helper.options.OptionsHandler;
+import modules.simulator.SimulatorHook;
 
 public class SmartValidator {
 
     public static void main(String args[]){
         OptionsHandler optionsHandler = OptionsHandler.getInstance();
         Feeder.getInstance().start();
+	(new SimulatorHook()).main(null);
     }
 }
--- src/main/java/modules/simulator/SimulatorHook.java
+++ src/main/java/modules/simulator/SimulatorHook.java
@@ -20,6 +20,14 @@ public class SimulatorHook implements Runnable {
 			s.executeUpdate("insert into archived_conflicts (prefix,reason) select prefix, route_validity from announcements inner join validated_roas_verified_announcements as o on announcements.id = verified_announcement_id where route_validity > 0 and not exists ( select verified_announcement_id from validated_roas_verified_announcements where route_validity = 0 and verified_announcement_id = o.verified_announcement_id ) group by prefix, route_validity;");
 			s.close();
 
+			/* create fake ROA - method = 0 */
+			s = dbc.createStatement();
+			s.executeUpdate("insert into archived_resolutions (prefix, asn, max_length, method) select prefix, asn, max_length, 0 from payload_roas as pr where not exists ( select id from validated_roas where prefix = pr.prefix and asn = pr.asn and max_length = pr.max_length );");
+
+			/* discard too restrictive ROA - method = 1 */
+			s = dbc.createStatement();
+			s.executeUpdate("insert into archived_resolutions (prefix, asn, max_length, method) select prefix, asn, max_length, 1 from validated_roas as vr where not exists ( select id from payload_roas where prefix = vr.prefix and asn = vr.asn and max_length = vr.max_length );");
+
 			dbc.commit();
 			dbc.close();
 		} catch (Exception ex) {
