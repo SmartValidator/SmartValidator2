697de9c73d7026dc8232e746b5f818c8570e7b12
yafim kazak <yafim.kazak@gmail.com>
2018-01-04 18:50:04 +0100

    Fix write of the RPKI validator to the router

--- src/main/java/modules/dataFeeder/rpkiFeeder/RpkiRepoDownloader.java
+++ src/main/java/modules/dataFeeder/rpkiFeeder/RpkiRepoDownloader.java
@@ -57,34 +57,37 @@ public class RpkiRepoDownloader implements Runnable {
                         assert dbConnection != null;
                         Statement stmt = dbConnection.createStatement();
 //                        ResultSet rs = stmt.executeQuery("CREATE TABLE validated_roas_rv_test ( id SERIAL (10) DEFAULT nextval('validated_roas_rv_test_id_seq':: REGCLASS ) NOT NULL\n" + "  CONSTRAINT validated_roas_rv_test_pkey\n" + "  PRIMARY KEY,\n" + "  asn          CIDR(max)    NOT NULL, max_length   INT4(10)     NOT NULL,  trust_anchor VARCHAR(255) NOT NULL\n" + "); COMMENT ON TABLE validated_roas_rv_test IS 'test table to be filled from the rpki validator run'");
-                        ResultSet rs = stmt.executeQuery("CREATE TABLE validated_roas_rv_test\n" +
+                        stmt.execute("DROP TABLE IF EXISTS rpki_validated_roas");
+                        stmt.execute("CREATE TABLE public.rpki_validated_roas\n" +
                                 "(\n" +
-                                "\troa_id INT4(10) NOT NULL\n" +
-                                "\t\tCONSTRAINT validated_roas_rv_test_pkey\n" +
-                                "\t\t\tPRIMARY KEY,\n" +
-                                "\tasn INT4(10) NOT NULL,\n" +
-                                "\tprefix CIDR(max) NOT NULL,\n" +
-                                "\tmax_length INT4(10),\n" +
-                                "\ttrust_anchor VARCHAR(255) NOT NULL);");
-                        dbConnection.commit();
+                                "  id INT DEFAULT nextval('validated_roas_id_seq'::REGCLASS) PRIMARY KEY NOT NULL,\n" +
+                                "  asn BIGINT NOT NULL,\n" +
+                                "  prefix CIDR NOT NULL,\n" +
+                                "  max_length INT NOT NULL,\n" +
+                                "  trust_anchor_id INT,\n" +
+                                "  created_at TIMESTAMP DEFAULT now() NOT NULL,\n" +
+                                "  CONSTRAINT validated_roas_trust_anchor_id_fkey FOREIGN KEY (trust_anchor_id) REFERENCES trust_anchors (id)\n" +
+                                ");\n");
 
 
-                        String insertStmt = "INSERT INTO validated_roas_rv_test VALUES (?, ?, ?, ?)";
+
+                        String insertStmt = "INSERT INTO rpki_validated_roas(asn,prefix,max_length,trust_anchor_id) VALUES (?, ?, ?, ?)";
                         PreparedStatement ps = dbConnection.prepareStatement(insertStmt);
 
                         for(Object value : roasArray  ){
                             JSONObject json_roa = (JSONObject) value;
                             ps.setInt(1, json_roa.getIntValue("asn"));
-
                             PGobject dummyObject = new PGobject();
                             dummyObject.setType("cidr");
                             dummyObject.setValue(json_roa.getString ("prefix"));
                             ps.setObject(2, dummyObject, Types.OTHER);
                             ps.setInt(3, json_roa.getIntValue("maxLength"));
-                            ps.setString(4, json_roa.getString("ta"));
+                            ps.setInt(4, 8);
+                            ps.addBatch();
+
 
                         }
-
+                        ps.executeBatch();
 
                     } catch (SQLException e) {
                         e.printStackTrace();
