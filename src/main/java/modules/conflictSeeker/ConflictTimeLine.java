dd5154ee909b09b5be5cbfbf044b463892d5acff
RefiPeretz <refi11991@gmail.com>
2018-01-05 11:01:37 +0200

    Fix type mistake :)

--- src/main/java/modules/conflictSeeker/ConflictTimeLine.java
+++ src/main/java/modules/conflictSeeker/ConflictTimeLine.java
@@ -39,8 +39,8 @@ public class ConflictTimeLine implements Runnable {
         try{
             ResultSet rs = connection.createStatement().executeQuery("SELECT COUNT(*) FROM validated_roas_verified_announcements WHERE route_validity = 2 OR route_validity = 1");
             if(rs.next()){
-                int count_conflits = rs.getInt("count");
-                connection.createStatement().execute("INSERT INTO conflict_timeline(count) VALUES ('" + count_conflits + "')");
+                int countConflicts = rs.getInt("count");
+                connection.createStatement().execute("INSERT INTO conflict_timeline(count) VALUES ('" + countConflicts + "')");
             }
 
         }catch(Exception e){
