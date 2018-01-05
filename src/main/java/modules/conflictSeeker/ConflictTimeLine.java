873ff5341302d6e8cc2954ab4e5226192ac462ec
RefiPeretz <refi11991@gmail.com>
2018-01-05 10:35:05 +0200

    Conflict timeline maintaining conflict table. Runnable object

--- src/main/java/modules/conflictSeeker/ConflictTimeLine.java
+++ src/main/java/modules/conflictSeeker/ConflictTimeLine.java
@@ -3,6 +3,7 @@ package modules.conflictSeeker;
 import modules.helper.DbHandler;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 
 public class ConflictTimeLine implements Runnable {
     Connection connection;
@@ -13,6 +14,17 @@ public class ConflictTimeLine implements Runnable {
 
     public void start(){
 //        SELECT COUNT(*) FROM validated_roas_verified_announcements WHERE route_validity = 2 OR route_validity = 1
+        try{
+            ResultSet rs = connection.createStatement().executeQuery("SELECT COUNT(*) FROM validated_roas_verified_announcements WHERE route_validity = 2 OR route_validity = 1");
+            if(rs.next()){
+                int count_conflits = rs.getInt("count");
+                connection.createStatement().execute("INSERT INTO conflict_timeline(count) VALUES ('" + count_conflits + "')");
+            }
+
+        }catch(Exception e){
+
+        }
+
 
 
     }
