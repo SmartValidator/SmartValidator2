24ff174f1e0ec5ab2f5b70db866fe9ed3fb7dcaa
Fabian Sauer <fabian.z.sauer@gmail.com>
2018-01-07 11:09:18 +0100

    Changed SQL statement to more efficient version

--- src/main/java/modules/dataFeeder/AnnouncementMerger.java
+++ src/main/java/modules/dataFeeder/AnnouncementMerger.java
@@ -12,19 +12,27 @@ public class AnnouncementMerger {
 
     private void mergeAnnouncementsTables() {
         try {
-            ResultSet rs = dbConnection.createStatement().executeQuery("SELECT asn, prefix FROM local_announcements");
-            String insertString = "INSERT INTO announcements (asn, prefix) VALUES (?, ?) " +
-                    "ON CONFLICT DO NOTHING";
-            PreparedStatement ps = dbConnection.prepareStatement(insertString);
-            while (rs.next()) {
-                ps.setLong(1, rs.getLong(1));
-                PGobject dummyObject = new PGobject();
-                dummyObject.setType("cidr");
-                dummyObject.setValue(rs.getString(2));
-                ps.setObject(2, dummyObject, Types.OTHER);
-                ps.addBatch();
-            }
-            ps.executeBatch();
+            dbConnection.createStatement().executeUpdate(
+                    "INSERT INTO" +
+                    " announcements(asn, prefix) (SELECT asn, prefix FROM local_announcements)" +
+                    " ON CONFLICT (asn, prefix)" +
+                    " DO UPDATE SET updated_at = now()"
+            );
+//            ResultSet rs = dbConnection.createStatement().executeQuery("SELECT asn, prefix FROM local_announcements");
+//            String insertString = "INSERT INTO" +
+//                    " announcements(asn, prefix) VALUES (?, ?)" +
+//                    " ON CONFLICT (asn, prefix)" +
+//                    " DO UPDATE SET updated_at = now()";
+//            PreparedStatement ps = dbConnection.prepareStatement(insertString);
+//            while (rs.next()) {
+//                ps.setLong(1, rs.getLong(1));
+//                PGobject dummyObject = new PGobject();
+//                dummyObject.setType("cidr");
+//                dummyObject.setValue(rs.getString(2));
+//                ps.setObject(2, dummyObject, Types.OTHER);
+//                ps.addBatch();
+//            }
+//            ps.executeBatch();
         } catch (SQLException e) {
             e.printStackTrace();
         }
